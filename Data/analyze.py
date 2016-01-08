3566d6dd1966ac0c3b32dad6698efa2b3bb388fa
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-01-08 13:35:16 +0100

    Add basic analysis script and readme

---
+++ Data/analyze.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datamatrix import plot as dmplot
+from datamatrix.operations import series
+from datamatrix.colors.tango import *
+from matplotlib import pyplot as plt
+import eyelinkparser
+
+# Use the eyelinkparser to get the data. For this basic analyis, we don't need
+# a custom parser class yet.
+dm = eyelinkparser.parse(cacheid='data')
+# Take the pupil trace during wmdelay, and turn it into a blink-reconstructed
+# trace of 3000 ms that is normalized relative to the first 100 ms of the
+# sample_stim trace.
+dm.pupil = dm.ptrace_wmdelay
+dm.pupil = series.blinkreconstruct(dm.pupil)
+dm.pupil.depth = 3000
+dm.pupil = series.baseline(dm.pupil, dm.ptrace_sample_stim, 0, 100)
+# Create a new plot, and plot both conditions as separate subplots.
+dmplot.new()
+for condition in [0, 1]:
+	plt.subplot(2,1,condition+1)
+	plt.title('Condition %d' % condition)
+	dm_dark = (dm.condition == condition) & (dm.backgroundcolor == 0)
+	dm_bright = (dm.condition == condition) & (dm.backgroundcolor == 255)
+	dmplot.trace(dm_dark.pupil, color=blue[1],
+		label='Attend dark (N=%d)' % len(dm_dark))
+	dmplot.trace(dm_bright.pupil, color=orange[1],
+		label='Attend bright (N=%d)' % len(dm_bright))
+	plt.xlabel('Time in retention interval (ms)')
+	plt.ylabel('Pupil size (norm.)')
+	plt.legend(loc='lower left')
+dmplot.save('pupil')
---
+++ Data/readme.md
@@ -0,0 +1,19 @@
+# Analysis recipe
+
+## Dependencies
+
+- Python (2.7 or 3.4)
+- numpy
+- scipy
+- [python-datamatrix]()
+- [python-eyelinkparser]()
+
+## Running the Analysis
+
+To run the analysis:
+
+	python analyze.py
+
+Some files are cached during the analysis. To clear the cache, run:
+
+	python analyze.py --clear-cache
