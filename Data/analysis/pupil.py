d47dc8cb8fc1287e3a0abcbcac2b1c371a38bbc6
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-02-17 16:24:56 +0100

    analysis update

---
+++ Data/analysis/pupil.py
@@ -0,0 +1,69 @@
+#-*- coding:utf-8 -*-
+
+"""
+This file is part of SWM_PUPIL.
+
+SWM_PUPIL is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SWM_PUPIL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SWM_PUPIL.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+from datamatrix import plot as dmplot
+from datamatrix import series, cached
+from datamatrix import operations as ops
+from datamatrix.colors.tango import *
+from matplotlib import pyplot as plt
+
+
+@cached
+def preprocess(dm):
+
+	# Extract subject ids from the first two letters of the source-asc basename
+	dm.sid = -1
+	for row in dm:
+		row.sid = os.path.basename(row.path)[:2]
+	# Take the pupil trace during wmdelay, and turn it into a blink-reconstructed
+	# trace of 3000 ms that is normalized relative to the first 100 ms of the
+	# sample_stim trace.
+	dm.pupil = dm.ptrace_wmdelay
+	print('wmdelay = %d' % dm.ptrace_wmdelay.depth)
+	print('sample_stim = %d' % dm.ptrace_sample_stim.depth)
+	dm.pupil = series.blinkreconstruct(dm.pupil)
+	dm.pupil.depth = 300
+	dm.pupil = series.baseline(dm.pupil, dm.ptrace_sample_stim, 0, 10)
+	return dm
+
+
+def pupil_plot(dm, suffix=''):
+
+	# Create a new plot, and plot both conditions as separate subplots.
+	dmplot.new()
+	for i, condition in enumerate(['L', 'O']):
+		plt.subplot(2,1,i+1)
+		plt.title('Condition %s' % condition)
+		dm_dark = (dm.condition == condition) & (dm.backgroundcolor == 0)
+		dm_bright = (dm.condition == condition) & (dm.backgroundcolor == 255)
+		dmplot.trace(dm_dark.pupil, color=blue[1],
+			label='Attend dark (N=%d)' % len(dm_dark))
+		dmplot.trace(dm_bright.pupil, color=orange[1],
+			label='Attend bright (N=%d)' % len(dm_bright))
+		plt.xlabel('Time in retention interval (ms)')
+		plt.ylabel('Pupil size (norm.)')
+		plt.legend(loc='lower left')
+	dmplot.save('pupil'+suffix)
+
+
+def subject_plot(dm):
+
+	for sid, dm_ in ops.split(dm.sid):
+		pupil_plot(dm_, 'pupil-%s' % sid)
--- Data/analyze.py
+++ Data/analyze.py
@@ -1,35 +1,31 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from datamatrix import plot as dmplot
-from datamatrix import series
-from datamatrix.colors.tango import *
-from matplotlib import pyplot as plt
+"""
+This file is part of SWM_PUPIL.
+
+SWM_PUPIL is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SWM_PUPIL is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SWM_PUPIL.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import eyelinkparser
+from datamatrix import dispatch
+from analysis import pupil, parse
 
-# Use the eyelinkparser to get the data. For this basic analyis, we don't need
-# a custom parser class yet.
-dm = eyelinkparser.parse(cacheid='data')
-
-# Take the pupil trace during wmdelay, and turn it into a blink-reconstructed
-# trace of 3000 ms that is normalized relative to the first 100 ms of the
-# sample_stim trace.
-dm.pupil = dm.ptrace_wmdelay
-dm.pupil = series.blinkreconstruct(dm.pupil)
-dm.pupil.depth = 3000
-dm.pupil = series.baseline(dm.pupil, dm.ptrace_sample_stim, 0, 100)
-# Create a new plot, and plot both conditions as separate subplots.
-dmplot.new()
-for i, condition in enumerate(['L', 'O']):
-	plt.subplot(2,1,i+1)
-	plt.title('Condition %s' % condition)
-	dm_dark = (dm.condition == condition) & (dm.backgroundcolor == 0)
-	dm_bright = (dm.condition == condition) & (dm.backgroundcolor == 255)
-	dmplot.trace(dm_dark.pupil, color=blue[1],
-		label='Attend dark (N=%d)' % len(dm_dark))
-	dmplot.trace(dm_bright.pupil, color=orange[1],
-		label='Attend bright (N=%d)' % len(dm_bright))
-	plt.xlabel('Time in retention interval (ms)')
-	plt.ylabel('Pupil size (norm.)')
-	plt.legend(loc='lower left')
-dmplot.save('pupil')
+# The waterfall resumes the analysis from the last point on that was not cached.
+dm = dispatch.waterfall(
+	(eyelinkparser.parse, 'data', {'parser' : parse.CustomParser}),
+	(pupil.preprocess, 'preprocess', {})
+	)
+# The dispatch automatically calls functions that have been specified on the
+# command line, like so: ./analyze.py @pupilplot @subject_plot
+dispatch.dispatch(dm, pupil)
